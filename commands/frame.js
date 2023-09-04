54dd74e31f5090a7ef30eedbd81898ba9847ba51
Charly McRae-Olson <colson32@gatech.edu>
2023-09-04 19:03:03 -0400

    changed command to a helper

--- commands/frame.js
+++ commands/frame.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('greetgbot')
+		.setName('helper')
 		.setDescription('says hello'),
 	async execute(interaction) {
-		await interaction.reply('Hi! My job is to record your art!');
+		await interaction.reply('Hi! My job is to record your art!\n\nIf you ping me on an image post, or a reply to an image post, I can add that image to my gallery.');
 	},
 };
