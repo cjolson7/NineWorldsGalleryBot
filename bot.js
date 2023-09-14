9882da499f15ab26e5213a45522b2d5017616d00
Charly McRae-Olson <colson32@gatech.edu>
2023-09-14 17:41:22 -0400

    updated edit on startup console log to actually count edits instead of individual logs

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, Collection, Events, GatewayIntentBits, GuildEmojiRoleManager, } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 const {data, helpers} = require('./data.js');
@@ -59,6 +59,7 @@ client.on("ready", () => {//when the bot first logs in
   console.log(`Logged in as ${client.user.tag}!`)
 
   //connect to message list file on startup and turn it into a list of discord links
+  var untrackedPosts = 0;//counter
   fs.readFile(helpers.filename, (err, contents) => {if(err) console.log(err);//log error if any
     var cachedLinks = contents.toString().replaceAll("\r","").split("\n");//trim and split to make neat list
       cachedLinks.forEach(async link => {//try each link
@@ -71,13 +72,14 @@ client.on("ready", () => {//when the bot first logs in
           try{cachedPost = await cachedChannel.messages.fetch(cachedMessageId);}catch{return};
           if(cachedPost.embeds.length<1 && cachedPost.attachments.size<1 && cachedPost.author.id == process.env.BOTID){
             //should be a bot post without art or embeds that is not in a gallery
+            untrackedPosts+=1
             await cachedPost.edit({content: data.genericEndMessage});
-            console.log("edited untracked post!")
           }
         }
       }
     })
-    //after processing it all, dump the file
+    //after processing it all, log the count and dump the file
+    console.log(`Edited ${untrackedPosts} untracked ` + untrackedPosts===1 ? "post" : "posts" + "!");
     fs.writeFile(helpers.filename, "", (err)=>{if(err) console.log(err);})//log error if any
   });
 })
