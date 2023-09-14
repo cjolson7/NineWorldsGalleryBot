f775d5b859dd051e065517d1f695284320608b8e
Charly McRae-Olson <colson32@gatech.edu>
2023-09-14 18:08:22 -0400

    tweaked wording more

--- commands/helper.js
+++ commands/helper.js
@@ -16,19 +16,24 @@ module.exports = {
 		const followupText = "You can click the buttons to learn more about how I work, or click Done.";
 		const doneText = "Alright! Call me if you need me!";
 		const editText = "If you call '/edit' and give me a link to one of your art posts in either of my galleries, I can fix it for you! "+
-			"Currently I can change or remove titles, descriptions, and spoiler tags. Note that remove and change are different - I have text inputs for new info, and True/False inputs for just removing it."+
+			"Currently I can change or remove titles, descriptions, and spoiler tags. "+
+			"Note that remove and change are different - I have text inputs for new info, and True/False inputs for just removing it."+
 			"If the piece is posted in both galleries, I will fix both posts (I do this using the links!). "+
-			"I can only edit my posts in my galleries, and only when /edit is called by the original artist credited in the gallery post.";
+			"I can only edit posts that I made in my galleries, and only when /edit is called by the original artist credited in the gallery post.";
 		const deleteText = "If you call '/delete' and give me a link to one of your art posts in one of my galleries, I can remove it for you! "+
-			`Currently, I remove any crossposted posts in both galleries, so if you accidentally shared it with Victoria, you'll need to repost it in <#${process.env.GALLERYCHANNELID}>`;
+			`Currently, I remove any crossposted posts in both galleries. `+
+			`If you accidentally shared it with Victoria, you'll need to ask me to repost it in <#${process.env.GALLERYCHANNELID}>`;
 		const spoilerText = "Sometimes I need clarification about what people want me to do with spoilers, to make sure my gallery is as well organized as possible. "+
 			"If you don't ask me to spoiler an image but it's spoilered in the original post, I'll offer to remove the spoiler. This won't change anything else about the image. "+
-			"If you do ask me to spoiler an image, I'll ask you to reply to me with a spoiler tag. Spoilers are more much helpful when people have info about when they're safe to click!"
+			"If you do ask me to spoiler an image, I'll ask you to reply to me with a spoiler tag. " +
+			"Spoilers are more helpful when people have info about when they're safe to click!"
 		const timeoutText = "When I post a response to an artist, I watch the emojis on that post for 48 hours or until I get a ✅ from the artist. "+
 			"If I don't have a 🇾 from the artist after 48 hours, I don't post anything. If I *do* get a 🇾 but the artist never clicks ✅, I post the art when I finish waiting."+
 			"If I need clarification about spoiler tags or unspoilering, I'll wait for 12 hours after asking and then post as-is if they didn't tell me anything more. " +
-			"I edit posts once I'm done watching them so that no one gets confused. Currently I lose track of any posts I was watching if I go down for maintenance, but I edit them all when I come back up!";
-		const userText = "I check discord user ids when I respond to art, and when I make gallery posts. Only the original poster of an image can consent for me to post it or ask me to edit or delete it. "+
+			"I edit posts once I'm done watching them so that no one gets confused. "+
+			"I lose track of the posts I'm watching if I go down for maintenance, but to make things nice and clear I edit them all to say so when I come back online!";
+		const userText = "I check discord user ids when I respond to art, and when I make gallery posts. "+
+			"Only the original poster of an image can consent for me to post it or ask me to edit or delete it. "+
 			"(The mods can always delete things, of course.) If someone else replies to or reacts to me when I'm waiting on the artist, I'll just ignore them.";
 
 		//set up array of explanations and buttonids for easy iteration (done is a special case and does not need to be in here)
