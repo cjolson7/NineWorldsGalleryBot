80a3970c8b750a2aff3bb07cba352a5e815a143a
Charly McRae-Olson <colson32@gatech.edu>
2026-01-20 20:08:58 -0500

    reorganize share function to distinguish between author and not author, in preparation for logic around asking author for sharing permission

--- commands/share.js
+++ commands/share.js
@@ -39,9 +39,13 @@ module.exports = {
 			});
 			return //end
 		}  
+		
+		//set up buttons used by bot interactions
+		const ask = new ButtonBuilder()
+			.setCustomId('ask')
+			.setLabel('Ask for Permission')
+			.setStyle(ButtonStyle.Primary)
 
-		//if not returned for error, ask them to confirm
-		//set up cancel buttons
 		const confirm = new ButtonBuilder()
 			.setCustomId('share')
 			.setLabel('Share With Victoria')
@@ -52,77 +56,120 @@ module.exports = {
 			.setLabel('Cancel')
 			.setStyle(ButtonStyle.Secondary);
 
-		const buttonRow = new ActionRowBuilder()
+		const askRow = new ActionRowBuilder()
+			.addComponents(ask, cancel);
+
+		const confirmRow = new ActionRowBuilder()
 			.addComponents(confirm, cancel);
 
-		await interaction.reply({
-			content: `Are you sure that you want to share [this post](${galleryLink}) to Victoria's gallery?`,
-			components: [buttonRow],
-			ephemeral: true
-		}).then((sent)=>{
-			const buttonCollector = sent.createMessageComponentCollector({ time: data.ephemeralTimeout });//30 min timeout
-			buttonCollector.on('collect', async buttonInteraction => { 
+			
+		// check whether the interactor is the original post author
+    	if (post.embeds[0].data.fields.find(f => f.name === "Artist").value.includes(interaction.user.id)) {
 
-				var buttonInteractionText;
+			//if they're the author, ask if they're sure, then post
+			await interaction.reply({
+				content: `Are you sure that you want to share [this post](${galleryLink}) to Victoria's gallery?`,
+				components: [confirmRow],
+				ephemeral: true
+			}).then((sent)=>{
 
-				if (buttonInteraction.customId === 'cancel') {
-					buttonInteractionText = "Alright, I won't share it!"
-				}
-				else if (buttonInteraction.customId === 'share') {
+				const buttonCollector = sent.createMessageComponentCollector({ time: data.ephemeralTimeout });//30 min timeout
+				buttonCollector.on('collect', async buttonInteraction => { 
 
-					var embedData = galleryPost.embeds[0].data //get original embed data
-					const timestamp = moment(embedData.timestamp).valueOf()//parse and convert to unix stamp for reuse
+					var buttonInteractionText;
 
-					const newEmbed = new EmbedBuilder() //preserve old data in embed format
-						.setColor(embedData.color)
-						.setTimestamp(timestamp)
-						.setFields(embedData.fields)
+					if (buttonInteraction.customId === 'cancel') {
+						buttonInteractionText = "Alright, I won't share it!"
+					}
+					else if (buttonInteraction.customId === 'share') {
 
-					//save description if one was present
-					if (embedData.description) newEmbed.setDescription(embedData.description)
+						victoriaLink = shareToVictoria(galleryPost)
 
-					//update links for new post
-					var originalLinks = embedData.fields.find(f => f.name === "Links" ).value;
-					const originalAndGalleryLinks = originalLinks+ ` / [Gallery](${galleryLink})`;
-
-					newEmbed.data.fields.find(f => f.name === "Links").value = originalAndGalleryLinks; //add links
-
-					//preserve images
-					var attachments = galleryPost.attachments;
-					imageFiles = [] 
-					attachments.forEach(async image => {
-						imageUrl = image.url,
-						filename = helpers.getFilenameFromLink(imageUrl)
-						imageFiles.push({
-								attachment: imageUrl,  
-							name: filename
-						})
-					});
-
-					//find victoria's gallery channel 
-					var victoriaLink
-					const victoriaChannel = await interaction.client.channels.cache.get(process.env.VICTORIACHANNELID);
-
-					//post to channel and get link
-					await victoriaChannel.send({
-						embeds: [newEmbed],
-						files: imageFiles,
-					}).then(sent => {
-						victoriaLink = helpers.generateLink(process.env.GUILDID, process.env.VICTORIACHANNELID, sent.id)
-					})
-
-					//format links
-					var originalAndNewLinks = originalLinks+ ` / [Victoria's Gallery](${victoriaLink})`;
-					
-					//edit old post with link
-					newEmbed.data.fields.find(f => f.name === "Links").value = originalAndNewLinks;
-					galleryPost.edit({ embeds: [newEmbed] });
-
-					buttonInteractionText = `Alright, how does that look? ${victoriaLink}`;
-				}
-				await buttonInteraction.update({ content: buttonInteractionText, components: [] })//remove buttons and update with comfirm text
-			});
+						buttonInteractionText = `Alright, how does that look? ${victoriaLink}`;
+					}
+					await buttonInteraction.update({ content: buttonInteractionText, components: [] })//remove buttons and update with confirm text
+				});
 				
-		}); 
+			});
+		}
+		else {
+			// if they're not the author, ask if they're sure, then ask for permission, then post
+			await interaction.reply({
+				content: `You are not the original author of [this post](${link}. If you still want me to share it with Victoria, I can ask the artist for permission. )`,
+				components: [askRow],
+				ephemeral: true
+			}).then((sent)=>{
+
+				const buttonCollector = sent.createMessageComponentCollector({ time: data.ephemeralTimeout });//30 min timeout
+				buttonCollector.on('collect', async buttonInteraction => {
+
+					var buttonInteractionText;
+					if (buttonInteraction.customId === 'ask') {
+						buttonInteractionText = "Okay, I've asked them!";
+
+						// code to go ask
+
+					}
+					else if (buttonInteraction.customId == 'cancel') {
+						buttonInteractionText = "Okay, I won't share the post with Victoria!";
+					}
+
+					await buttonInteraction.update({ content: buttonInteractionText, components: [] })//remove buttons and update with confirm text
+				});
+			});
+		}
 	}
+}
+
+function shareToVictoria(galleryPost) {
+
+	var embedData = galleryPost.embeds[0].data //get original embed data
+	const timestamp = moment(embedData.timestamp).valueOf()//parse and convert to unix stamp for reuse
+
+	const newEmbed = new EmbedBuilder() //preserve old data in embed format
+		.setColor(embedData.color)
+		.setTimestamp(timestamp)
+		.setFields(embedData.fields)
+
+	//save description if one was present
+	if (embedData.description) newEmbed.setDescription(embedData.description)
+
+	//update links for new post
+	var originalLinks = embedData.fields.find(f => f.name === "Links" ).value;
+	const originalAndGalleryLinks = originalLinks+ ` / [Gallery](${galleryLink})`;
+
+	newEmbed.data.fields.find(f => f.name === "Links").value = originalAndGalleryLinks; //add links
+
+	//preserve images
+	var attachments = galleryPost.attachments;
+	imageFiles = [] 
+	attachments.forEach(async image => {
+		imageUrl = image.url,
+		filename = helpers.getFilenameFromLink(imageUrl)
+		imageFiles.push({
+				attachment: imageUrl,  
+			name: filename
+		})
+	});
+
+	//find victoria's gallery channel 
+	var victoriaLink
+	const victoriaChannel = await interaction.client.channels.cache.get(process.env.VICTORIACHANNELID);
+
+	//post to channel and get link
+	await victoriaChannel.send({
+		embeds: [newEmbed],
+		files: imageFiles,
+	}).then(sent => {
+		victoriaLink = helpers.generateLink(process.env.GUILDID, process.env.VICTORIACHANNELID, sent.id)
+	})
+
+	//format links
+	var originalAndNewLinks = originalLinks+ ` / [Victoria's Gallery](${victoriaLink})`;
+	
+	//edit old post with link
+	newEmbed.data.fields.find(f => f.name === "Links").value = originalAndNewLinks;
+	galleryPost.edit({ embeds: [newEmbed] });
+
+	return victoriaLink
 }
